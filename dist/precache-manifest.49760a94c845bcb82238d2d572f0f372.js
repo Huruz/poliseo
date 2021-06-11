9c970174a22f15c872014558ab135556d735bfaa
Huruz <huruz_wwe@hotmail.com>
2021-06-10 19:14:53 -0500

    Initial dist subtree commit

---
+++ dist/precache-manifest.49760a94c845bcb82238d2d572f0f372.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01900a2e130d26dc8155",
+    "url": "/poliseo/css/app.e130d58d.css"
+  },
+  {
+    "revision": "3f8caecd5a05f2527f87b36b60d56a86",
+    "url": "/poliseo/img/blastoise.3f8caecd.png"
+  },
+  {
+    "revision": "c9f736f48d07f53f9523e1afdfcb1395",
+    "url": "/poliseo/img/charizard.c9f736f4.png"
+  },
+  {
+    "revision": "76f027c1d8ea2170cedd976017330331",
+    "url": "/poliseo/img/pikachu.76f027c1.png"
+  },
+  {
+    "revision": "195345afd443e6fe90f6a400bc7fd82a",
+    "url": "/poliseo/img/venasaur.195345af.png"
+  },
+  {
+    "revision": "19bd865de8b3478ddedc99ff310161cd",
+    "url": "/poliseo/index.html"
+  },
+  {
+    "revision": "2e83b484c5a38d9f6cf0",
+    "url": "/poliseo/js/about.18fc1036.js"
+  },
+  {
+    "revision": "01900a2e130d26dc8155",
+    "url": "/poliseo/js/app.881d960a.js"
+  },
+  {
+    "revision": "353b0ec63eb790cf1d3e",
+    "url": "/poliseo/js/chunk-vendors.78b87b94.js"
+  },
+  {
+    "revision": "d0134630b84b73d82b0e8b8f59b56127",
+    "url": "/poliseo/js/keypress.js"
+  },
+  {
+    "revision": "6488e3b1d1e14f49091beadd933a895c",
+    "url": "/poliseo/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/poliseo/robots.txt"
+  },
+  {
+    "revision": "ef0739ecd739149b23e43097c4350c31",
+    "url": "/poliseo/tailwind.css"
+  }
+]);
